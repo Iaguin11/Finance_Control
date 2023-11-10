847b628e39be4976725f881871290852c54217ab
Iago Novaes <iagonovaes19@gmail.com>
2023-11-09 22:16:46 -0300

    project: visual-structure

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,23 @@
+import { NewTransactionModal } from "../NewTransactionModal";
 import { HeaderContainer, HeaderContext, NewTransactionButton } from "./styles";
+import * as Dialog from "@radix-ui/react-dialog"
 
 export function Header(){
   return(
     <HeaderContainer>
       <HeaderContext>
         <span>Logo</span>
-        <NewTransactionButton>
-          Nova Transação
-        </NewTransactionButton>
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>
+              Nova Transação
+            </NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal/>
+        </Dialog.Root>
+        
       </HeaderContext>
     </HeaderContainer>
   )
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,55 @@
+import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
+
+interface Transaction {
+  id: number
+  description: string
+  type: 'income' | 'outcome'
+  price: number
+  category: string
+  createdAt: string
+}
 
 export function Transactions(){
+  const [transaction, setTransaction] = useState<Transaction[]>([])
+
+  async function loadTransaction() {
+    const response = await fetch('http://localhost:3000/transaction')
+    const data = await response.json()
+    setTransaction(data)
+  }
+  useEffect(()=>{
+    loadTransaction()
+  },[])
+
   return(
     <div>
       <Header/>
       <Summary/>
+
+      <TransactionsContainer>
+        <SearchForm/>
+        
+        <TransactionsTable>
+          <tbody>
+           {transaction.map(transaction => (
+            <tr key={transaction.id}>
+              <td width="50%">{transaction.description}</td>
+              <td>
+                <PriceHighlight variant={transaction.type}>
+                  {transaction.price}
+                </PriceHighlight>
+              </td>
+              <td>{transaction.category}</td>
+              <td>{transaction.createdAt}</td>
+           </tr>
+           ))}
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
